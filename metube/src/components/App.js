262a95b410fb5df6b3e1e3f4308b28acff6e58fe
mosidrum <mosiokanga@gmail.com>
2023-01-11 16:10:29 +0100

    metube project done

--- metube/src/components/App.js
+++ metube/src/components/App.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import SearchBar from './SerchBar';
+import React from "react";
+import SearchBar from "./SerchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
 
-    onInputTextSubmit = (inputText) => {
-        console.log(inputText);
+  componentDidMount() {
+    this.onInputTextSubmit('youtube');
+  }
 
-    }
+  onInputTextSubmit = async (inputText) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: inputText,
+      },
+    });
+    this.setState({ videos: response.data.items,
+        selectedVideo: response.data.items[0]
+     });
+  };
 
-    render() {
-        return (
-            <div className='ui container'>
-                <SearchBar onFormSubmit={this.onInputTextSubmit} />
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onInputTextSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        );
-    }
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- metube/src/components/SerchBar.js
+++ metube/src/components/SerchBar.js
@@ -10,20 +10,20 @@ class SearchBar extends React.Component {
     };
 
     onFormSubmit = (event) => {
-        event.preventDefult();
+        event.preventDefault();
 
        this.props.onFormSubmit(this.state.inputText);
     };
 
     render () {
       return(
-        <div className='search-bar ui segment' onSubmit={this.onFormSubmit}>
-           <div className='ui form'>
+        <div className='search-bar ui segment'>
+           <form onSubmit={this.onFormSubmit} className='ui form'>
             <div className='field'>
             <label className='search'>Search Videos</label>
                     <input type="text" value={this.state.inputText} placeholder="Search..." onChange={this.onInputChange} />
             </div>
-           </div>
+           </form>
         </div>
       );
     }
