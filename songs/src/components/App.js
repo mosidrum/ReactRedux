dbb3aa8c1793d816d53c266f1b88c95a492d63d9
mosidrum <mosiokanga@gmail.com>
2023-01-15 17:53:06 +0100

    continued songs project

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM  from 'react-dom';
+import React from "react";
 
-class App extends React.Component {
-  render () {
+
+const App = () => {
+
     return <div>App</div>;
-  }
+
 }
 
 export default App;
---
+++ songs/src/reducers/index.js
@@ -0,0 +1,20 @@
+const songsReducer = () => {
+    return [
+        {
+            title: 'No Scrubs',
+            duration : '4:31'
+        },
+        {
+            title: 'Arise',
+            duration : '6:29'
+        },
+        {
+            title: 'Increased Glory',
+            duration : '5:55'
+        },
+        {
+            title: 'Excess Love',
+            duration : '6:06'
+        }
+    ];
+};
