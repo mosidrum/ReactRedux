b7d6b242ef7c8f6fa2e6407b7fb5d99f0f7d23ab
mosidrum <mosiokanga@gmail.com>
2023-01-18 00:20:08 +0100

    started another project blog

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -7,8 +7,9 @@ const SongDetail = ({ mySelectedSong }) => {
   } else {
     return (
       <div>
-        <h5>Title:<h3>{mySelectedSong.title} <br />
-        Duration: { mySelectedSong.duration}</h3></h5>
+        <h5>Title:
+           {mySelectedSong.title} <br />
+        Duration: { mySelectedSong.duration}</h5>
       </div>
 
     );
