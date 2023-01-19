f98e867a753cc17f818915aa34ec7991140815b8
mosidrum <mosiokanga@gmail.com>
2023-01-19 17:18:47 +0100

    i am not really understanding redux

---
+++ blog/src/component/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PostList from "./PostList";
+
+const App = () => {
+  return <div className="ui container">
+    <div><PostList /></div>
+  </div>;
+};
+
+export default App;
