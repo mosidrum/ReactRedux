5e139b7530417ba7371a33178afa02893bf5e07d
mosidrum <mosiokanga@gmail.com>
2023-01-29 19:39:09 +0100

    restarting the whole prject from the start

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,26 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import StreamCreate from "./streams/StreamCreate";
-import StreamEdit from "./streams/StreamEdit";
-import StreamDelete from "./streams/StreamDelete";
-import StreamList from "./streams/StreamList";
-import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Header!!!</h1>
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
-          <Route path="/streams/show" exact component={StreamShow} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
--- streams/client/src/components/streams/StreamEdit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
-};
-
-export default StreamEdit;
